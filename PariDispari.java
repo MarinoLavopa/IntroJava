670379ca7105fac9f7d550caf6e719268030f76b
MarinoLavopa <marinolavopa@hotmail.it>
2026-03-19 16:26:43 +0100

    fine esercitazione introJava

--- PariDispari.java
+++ PariDispari.java
@@ -13,49 +13,57 @@ public class PariDispari {
         int conteggioPari=0;
         int conteggioDispari=0;
         int [] numList= new int[contatore];
-
+        
         
         for(int i=0; i < contatore; i++){
             System.out.println("Inserisci un Numero: ");
-                int NumUtente=scanner.nextInt();
-
-                numList[i]=NumUtente; 
-
+            int NumUtente=scanner.nextInt();
+            
+            numList[i]=NumUtente; 
+            
+            
+            if(NumUtente %2 == 0){
+                sommaPari=sommaPari+ NumUtente;
+                sommaTotale=sommaTotale + NumUtente;
+                conteggioTotale++;
+                conteggioPari++;
+                System.out.println("Il numero "+ NumUtente+ " è Pari! ");
                 
-                if(NumUtente %2 == 0){
-                     sommaPari=sommaPari+ NumUtente;
-                     sommaTotale=sommaTotale + NumUtente;
-                     conteggioTotale++;
-                     conteggioPari++;
-                    System.out.println("Il numero "+ NumUtente+ " è Pari! ");
-                    
-                }else{
-                    sommaDispari=sommaDispari+NumUtente;
-                    sommaTotale=sommaTotale+NumUtente;
-                    conteggioTotale++;
-                    conteggioDispari++;
-                    System.out.println("Il numero "+ NumUtente+ " è Dispari! ");
-                    
-                }
-    }
+            }else{
+                sommaDispari=sommaDispari+NumUtente;
+                sommaTotale=sommaTotale+NumUtente;
+                conteggioTotale++;
+                conteggioDispari++;
+                System.out.println("Il numero "+ NumUtente+ " è Dispari! ");
+                
+            }
+        }
+        System.out.println("Gli elementi in posizione pari dell'array sono:");
+        for(int i=0; i < contatore; i+=2){
+            //elementi in posizione pari dell' array
+            
+            System.out.println(numList[i]);
+        }
+        double mediaPari=0;
+        double mediaDispari=0;
+        double mediaTotale=0;
+        
+        mediaTotale= (double)sommaTotale/conteggioTotale;
+        mediaPari= (double)sommaPari/conteggioPari;
+        mediaDispari=(double)sommaDispari/conteggioDispari;
+        
+        System.out.println("La media dei numeri pari è "+ mediaPari+ ",mentre per i numeri dispari la media è "+ mediaDispari+ "per una media totale di: "+ mediaTotale );
+        
+        //vigliamo sapere l'elemento maggiore dell' array
+        int maxnum=numList[0];
+        for(int i=0; i < contatore; i++){
+            if(numList[i]> maxnum){
+                maxnum=numList[i];
+            }
+        }
+        System.out.println("Il numero più alto è : "+maxnum+ ", mentre la somma dei numeri inseriti è :"+ sommaTotale);
 
-    double mediaPari=0;
-    double mediaDispari=0;
-    double mediaTotale=0;
-
-    mediaTotale= (double)sommaTotale/conteggioTotale;
-    mediaPari= (double)sommaPari/conteggioPari;
-    mediaDispari=(double)sommaDispari/conteggioDispari;
-
-    System.out.println("La media dei numeri pari è "+ mediaPari+ ",mentre per i numeri dispari la media è "+ mediaDispari+ "per una media totale di: "+ mediaTotale );
-      
-    for(int i=0; i < contatore; i+=2){
-        //elementi in posizione pari dell' array
-        System.out.println("elementi in posizione pari dell'array");
-
-        System.out.println(numList[i]);
-    }   
-    scanner.close();
+        scanner.close();
         
     }
     
